3ee7fb86dc76ffb9c9de6b5b369188a802433944
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-03-22 17:26:17 +0100

    Create PatternPatcherIsolateProcess.h

---
+++ PatternPatcherIsolateProcess.h
@@ -0,0 +1,21 @@
+//this is not intuitive to patch z vode.
+//then, it is best to take two roadmaps.
+//so the pattern seems like z dispatcher.
+//znd it can help javascript langage if im not wrong
+//by rnhancing the threads behaviours.
+
+//nowadays technologies are able to handle this.
+//but using the cpu with both signals is do'e jere.
+//it helps force chips that can help hash signals.
+class NotSafrThreadEnableSignals : piblic cpudispatcher
+{
+public:
+  ....(hashsignal hs);
+
+  //lets do another thing here and not copy paste
+  //the previous projects. i dont remember which one rely on this behaviour.
+  void waitorpollsignals();
+}
+
+//if the thread is safe, there is a way
+//to check its id. i think so. accordi'g to heat transfer.
