07a72d3a191f27075bdeac06aeaee0275b7b4b03
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-03-23 14:32:51 +0100

    Create DualNeuralNetworkMainIsolateProcess.h

---
+++ DualNeuralNetworkMainIsolateProcess.h
@@ -0,0 +1,6 @@
+//the networks must handle diffeomorohism
+//at first side because we must omit
+//values checking later  it means now
+//that hidden layers are supoosed tonreact
+//to some tresholds. mathematically soeaking
+//it means...
